eb0aa8595c62086dad9778d49263fee3b9624d13
Nickolai Grachev <nickbd95@gmail.com>
2024-02-16 13:07:42 +0300

    app done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,67 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var player: AVAudioPlayer?
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var counter = 60
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        progressBar.progress = 0
+    }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        progressBar.progress = 0
+        secondsPassed = 0
+        let hardness =  sender.currentTitle ?? ""
+        titleLabel.text = hardness
+        totalTime = eggTimes[hardness] ?? 0
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc
+    func updateCounter() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+        } else {
+            timer.invalidate()
+            titleLabel.text = "It's time!"
+            playSound()
+        }
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
 
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
 
+    
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
+
+
